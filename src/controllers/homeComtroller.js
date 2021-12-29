9cde32ee9d375694deaa294b0d11f1117c75e42b
congthai99 <thaicong328@gmail.com>
2021-12-29 15:53:15 +0700

    video 25 create a new user

--- src/controllers/homeComtroller.js
+++ src/controllers/homeComtroller.js
@@ -1,5 +1,5 @@
 import db from '../models/index'
-
+import CRUDService from "../services/CRUDService"
 
 let getHomePage = async(req, res) => {
     try {
@@ -15,9 +15,22 @@ let getHomePage = async(req, res) => {
 
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
-
 }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async(req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    console.log(message)
+    return res.send('postCRUD from server')
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,10 +5,10 @@ let router = express.Router();
 // viet tat ca cac router 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
-
     router.get('/about', homeController.getAboutPage);
+    router.get('/crud', homeController.getCRUD);
 
-
+    router.post('/post-crud', homeController.postCRUD);
 
     return app.use("/", router)
 }
